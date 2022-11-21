5b5da7ba17df56eea59ae4623855244c92933e41
Eduardo Granados <e.granados2610@gmail.com>
2022-11-21 18:55:17 -0500

    Agregando offcanvas

--- main.js
+++ main.js
@@ -74,20 +74,18 @@ verCarrito.addEventListener("click", () => {
 const mostrarCarrito = () => {
   contenedorCarrito.innerHTML = "";
   carrito.forEach((producto) => {
-    const card = document.createElement("div");
-    card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
-    card.innerHTML = `
-        <div class="card">
-        <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
-            <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">Precio: ${producto.precio}</p>
+    const cardc = document.createElement("div");
+    cardc.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+    cardc.innerHTML = `
+        <div class="cardc">
+        <img src="${producto.img}" class="card-img-top imgCarrito" alt="${producto.nombre}">
+            <div class="cardc-body">
             <p class="card-text">Cantidad: ${producto.cantidad}</p>
             <button class="btn colorBoton" id="eliminar${producto.id}"> Eliminar Producto </button>
             </div>
         </div>   
     `;
-    contenedorCarrito.appendChild(card);
+    contenedorCarrito.appendChild(cardc);
 
     //Eliminar productos del carrito:
 
